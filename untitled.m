6550c9665d84e66eb1cab83634d14a428fe51f4d
Eden Bental <edeni.dooni@gmail.com>
2022-11-07 18:36:22 +0200

    use the script for random bits and bases

--- untitled.m
+++ untitled.m
@@ -1,1 +1,7 @@
 disp('hi')
+numbers=2
+bit_length=5
+alice_bit=randi(numbers,bit_length)
+alice_base=randi(numbers,bit_length)
+eve_base=randi(numbers,bit_length)
+bob_base=randi(numbers,bit_length)
